fec046d5aa058f4128ca073d0779d7899751915d
Gordon French <frenchsquared@gmail.com>
2018-05-08 11:47:43 -0600

    Import Counter

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -8,8 +8,7 @@ class Counter extends React.Component {
     }
     render(){
         return <div>
-                    <h1>Counter</h1>
-                    <Link to="/">Home</Link> | <Link to="/page-2">Page 2</Link> | <Link to="/counter">Counter</Link><br/>
+                    <p>I am a color variable passed in: {this.props.color}</p>
                     <p>current count: {this.state.count}</p>
                     <button onClick={() => this.setState( {count: this.state.count + 1} )} >plus</button>
                     <button onClick={() => this.setState( {count: this.state.count - 1} )} >minus</button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Link from "gatsby-link"
+import Counter from "./counter"
 
 export default () => <div style={{color: 'red'}}>
                         <h1>Hello world!</h1>
                         <p>this is a paragraph</p>
-                        <Link to="/">Home</Link> | <Link to="/page-2">Page 2</Link> | <Link to="/counter">Counter</Link><br/>
+                        <Link to="/">Home</Link> | <Link to="/page-2">Page 2</Link><br/>
+                        <hr/>
+                        <Counter color="blue" />
                         <hr/>
                         <img src="http://gordonfrench.com/15-Z28/right-side.jpg"/>
                         <p>npm run build</p>
